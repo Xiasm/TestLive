d1526ec6cccb80f72130eec8a41dc532bbe61404
xsm <xsm0824mn003@163.com>
2016-11-06 00:17:00 +0800

    我的界面部分完成，要睡觉了。

--- MLive/app/src/main/java/xsm/org/mlive/fragments/MineFragment.java
+++ MLive/app/src/main/java/xsm/org/mlive/fragments/MineFragment.java
@@ -1,30 +1,43 @@
 package xsm.org.mlive.fragments;
 
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawable;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.target.BitmapImageViewTarget;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import xsm.org.mlive.R;
+import xsm.org.mlive.tools.GlideCircleTransform;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class MineFragment extends Fragment {
 
-
+    @BindView(R.id.mine_picture) ImageView mIngPicture;
     public MineFragment() {
         // Required empty public constructor
     }
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_mine, container, false);
+        View ret = inflater.inflate(R.layout.fragment_mine, container, false);
+        ButterKnife.bind(this, ret);
+        Glide.with(getContext()).load(R.mipmap.popular_picture).transform(new GlideCircleTransform(getContext())).into(mIngPicture);
+
+        return ret;
     }
 
 }
